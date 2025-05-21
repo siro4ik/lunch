5430e3657922f7f0f3696a13ac158afa0d68f626
siro4ik <deputatov0@mail.ru>
2025-05-21 19:59:49 +0300

    +

--- script.js
+++ script.js
@@ -5,5 +5,20 @@ const confirmBtn = document.getElementById('confrmBtn');
 confirmBtn.addEventListener('click',()=>{
     const startTime = startTimePicker.value;
     const endTime = endTimePicker.value;
-    alert(`Вы выбрали: ${startTime} - ${endTime}`);
+
+    const startMinutes = convertTimeToMinutes(startTime);
+    const endMinutes = convertTimeToMinutes(endTime);
+
+    if(endMinutes  - startMinutes > 60){
+        alert ('Нельзя перегуливать!')
+    }else{
+        alert(`Вы выбрали: ${startTime} - ${endTime}`);
+    }
 });
+
+function convertTimeToMinutes(timeStr){
+    const [hours, minutes] = timeStr.split(':').map(Number);
+    return hours * 60 + minutes;
+};
+
+
