ea13f119a8329de26dd62ce1dd7e89b468d4d179
siro4ik <deputatov0@mail.ru>
2025-05-21 23:30:14 +0300

    +

--- script.js
+++ script.js
@@ -1,24 +1,53 @@
-const startTimePicker = document.getElementById('startTimePicker');
-const endTimePicker = document.getElementById('endTimePicker');
-const confirmBtn = document.getElementById('confrmBtn');
-
-confirmBtn.addEventListener('click',()=>{
-    const startTime = startTimePicker.value;
-    const endTime = endTimePicker.value;
-
-    const startMinutes = convertTimeToMinutes(startTime);
-    const endMinutes = convertTimeToMinutes(endTime);
-
-    if(endMinutes  - startMinutes > 60){
-        alert ('Нельзя перегуливать!')
-    }else{
-        alert(`Вы выбрали: ${startTime} - ${endTime}`);
-    }
-});
-
-function convertTimeToMinutes(timeStr){
-    const [hours, minutes] = timeStr.split(':').map(Number);
-    return hours * 60 + minutes;
+window.onload = function() {
+    createTimeSlots();
 };
+
+
+function createTimeSlots(){
+    const table = document.getElementById('slots');
+
+    for(let hour = 0; hour<24; hour++){
+        for ( let minute = 0; minute < 60; minute+=30){
+            const row = document.createElement('tr');
+
+            const timeCell = document.createElement('td');
+            timeCell.textContent = `${hour.toString().padStart(2, '0')}:${minute.toString().padStart(2, '0')}`;
+            row.appendChild(timeCell)
+
+            for (let day = 0; day<7; day++){
+                const cell = document.createElement('td');
+                cell.id = `d${day}h${hour}m${minute}`;
+                row.appendChild(cell);
+            }
+            table.appendChild(row);
+        }
+    }
+}
+
+function addLunch (){
+    const startInput = document.getElementById('start');
+    const endInput = document.getElementById('end');
+
+    const start = startInput.value;
+    const end = endInput.value;
+
+    const today = new Date().getDay();
+
+    const [startHour] = start.split(':').map(Number);
+    const [endHour] = end.split(':').map(Number);
+
+    if(start >= end){
+        alert('Ты че делаешь?');
+        return;
+    }
+
+    const cell = document.getElementById(`d${today}h${startHour}`);
+    const userName = document.getElementById('userName').value;
+
+    if (cell) {
+        cell.classList.add('lunch-time');
+        cell.textContent = `${userName}: ${start}-${end}`;
+    }
+}
 
 
