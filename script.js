c1793bbd681f43f501da650531a61eb7f6059376
siro4ik <deputatov0@mail.ru>
2025-05-22 01:17:49 +0300

    +

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAy9OEzVWlqWTGwzpcV5847sugtmvOYedU",
+    authDomain: "lunch-575f4.firebaseapp.com",
+    databaseURL: "https://lunch-575f4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "lunch-575f4",
+    storageBucket: "lunch-575f4.firebasestorage.app",
+    messagingSenderId: "346613736019",
+    appId: "1:346613736019:web:17493f838e994d28e65ca6",
+    measurementId: "G-N5V8E7TV4D"
+};
+
+
 window.onload = createTimeSlots;
 
 function createTimeSlots() {
