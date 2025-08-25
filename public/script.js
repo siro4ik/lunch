fa021fbcacc9d8826ad4020a43a63797651bcf89
Igor <112963829+siro4ik@users.noreply.github.com>
2025-08-25 17:33:49 +0300

    проверка времени каждую минуту

--- public/script.js
+++ public/script.js
@@ -522,6 +522,8 @@ document.addEventListener('DOMContentLoaded', () => {
     loadLunches();
     updateCurrentTimeLine();
     setInterval(updateCurrentTimeLine, 30000);
+    checkLunchTime();
+    setInterval(checkLunchTime, 60000);
 });
 
 // Обработчики изменения размера окна
