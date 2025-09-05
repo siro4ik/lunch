d94f8abec0d329f68c4f79e97e6e1953220a8fef
Igor <112963829+siro4ik@users.noreply.github.com>
2025-09-05 15:25:33 +0300

    начало скрипта ЗЗ

--- public/script.js
+++ public/script.js
@@ -499,6 +499,23 @@ function toggleRainVisibility(visible) {
 //     }
 // }
 
+// ОПРЕДЕЛЕНИЕ ЗЗ/ВОЗРАСТА
+
+// const zodiacSigns = [
+//     { name: "Овен", start: { month: 3, day: 21 }, end: { month: 4, day: 20 } },
+//     { name: "Телец", start: { month: 4, day: 21 }, end: { month: 5, day: 20 } },
+//     { name: "Близнецы", start: { month: 5, day: 21 }, end: { month: 6, day: 21 } },
+//     { name: "Рак", start: { month: 6, day: 21 }, end: { month: 7, day: 22 } },
+//     { name: "Лев", start: { month: 7, day: 23 }, end: { month: 8, day: 22 } },
+//     { name: "Дева", start: { month: 8, day: 23 }, end: { month: 9, day: 22 } },
+//     { name: "Весы", start: { month: 9, day: 23 }, end: { month: 10, day: 22 } },
+//     { name: "Скорпион", start: { month: 10, day: 23 }, end: { month: 11, day: 21 } },
+//     { name: "Стрелец", start: { month: 11, day: 22 }, end: { month: 12, day: 21 } },
+//     { name: "Козерог", start: { month: 12, day: 22 }, end: { month: 1, day: 19 } },
+//     { name: "Водолей", start: { month: 1, day: 20 }, end: { month: 2, day: 18 } },
+//     { name: "Рыбы", start: { month: 2, day: 19 }, end: { month: 3, day: 20 } }
+// ];
+
 // Инициализация при загрузке страницы
 document.addEventListener('DOMContentLoaded', () => {
     const isDarkTheme = localStorage.getItem('darkTheme') === 'true';
