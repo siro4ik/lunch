69513d445b3dd5a50a6ecea09a3e4318408b402a
Igor <112963829+siro4ik@users.noreply.github.com>
2025-09-10 21:33:11 +0300

    +

--- public/main.js
+++ public/main.js
@@ -504,20 +504,7 @@ const dateInput = document.querySelector ("#birthDate")
 
 // ОПРЕДЕЛЕНИЕ ЗЗ/ВОЗРАСТА
 
-const zodiacSigns = [
-    { name: "Овен", start: { month: 3, day: 21 }, end: { month: 4, day: 20 } },
-    { name: "Телец", start: { month: 4, day: 21 }, end: { month: 5, day: 20 } },
-    { name: "Близнецы", start: { month: 5, day: 21 }, end: { month: 6, day: 21 } },
-    { name: "Рак", start: { month: 6, day: 22 }, end: { month: 7, day: 22 } },
-    { name: "Лев", start: { month: 7, day: 23 }, end: { month: 8, day: 23 } },
-    { name: "Дева", start: { month: 8, day: 24 }, end: { month: 9, day: 23 } },
-    { name: "Весы", start: { month: 9, day: 24 }, end: { month: 10, day: 23 } },
-    { name: "Скорпион", start: { month: 10, day: 24 }, end: { month: 11, day: 22 } },
-    { name: "Стрелец", start: { month: 11, day: 23 }, end: { month: 12, day: 21 } },
-    { name: "Козерог", start: { month: 12, day: 22 }, end: { month: 1, day: 20 } },
-    { name: "Водолей", start: { month: 1, day: 21 }, end: { month: 2, day: 20 } },
-    { name: "Рыбы", start: { month: 2, day: 21 }, end: { month: 3, day: 20 } }
-];
+
 
 // Парсинг строки, удаление ненужных символов, проверка на число и на корректность ввода, относительно промежутка чисел
 
---
+++ public/modules/zodiac.js
@@ -0,0 +1,14 @@
+export const zodiacSigns = [
+    { name: "Овен", start: { month: 3, day: 21 }, end: { month: 4, day: 20 } },
+    { name: "Телец", start: { month: 4, day: 21 }, end: { month: 5, day: 20 } },
+    { name: "Близнецы", start: { month: 5, day: 21 }, end: { month: 6, day: 21 } },
+    { name: "Рак", start: { month: 6, day: 22 }, end: { month: 7, day: 22 } },
+    { name: "Лев", start: { month: 7, day: 23 }, end: { month: 8, day: 23 } },
+    { name: "Дева", start: { month: 8, day: 24 }, end: { month: 9, day: 23 } },
+    { name: "Весы", start: { month: 9, day: 24 }, end: { month: 10, day: 23 } },
+    { name: "Скорпион", start: { month: 10, day: 24 }, end: { month: 11, day: 22 } },
+    { name: "Стрелец", start: { month: 11, day: 23 }, end: { month: 12, day: 21 } },
+    { name: "Козерог", start: { month: 12, day: 22 }, end: { month: 1, day: 20 } },
+    { name: "Водолей", start: { month: 1, day: 21 }, end: { month: 2, day: 20 } },
+    { name: "Рыбы", start: { month: 2, day: 21 }, end: { month: 3, day: 20 } }
+];
