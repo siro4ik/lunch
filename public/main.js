1134042546b2c58e24ef0ae3c50192f5c48acdd5
Igor <112963829+siro4ik@users.noreply.github.com>
2025-09-11 00:26:49 +0300

    +

--- public/main.js
+++ public/main.js
@@ -1,20 +1,11 @@
 import { zodiacSigns, parseDate, getZodiacSign, calculateAge } from "./modules/zodiac.js";
-
-
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAy9OEzVWlqWTGwzpcV5847sugtmvOYedU",
-    authDomain: "lunch-575f4.firebaseapp.com",
-    databaseURL: "https://lunch-575f4-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "lunch-575f4",
-    storageBucket: "lunch-575f4.firebasestorage.app",
-    messagingSenderId: "346613736019",
-    appId: "1:346613736019:web:17493f838e994d28e65ca6",
-    measurementId: "G-N5V8E7TV4D"
-};
+import { firebaseConfig } from './modules/firebase.js';
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
+
+
+
 
 // сокрытие элемента при скролле
 const hide = document.querySelector(".element-to-hide");
