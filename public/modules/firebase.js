f5891ac23dcf4492d2bfcc751d549fccf4c68491
Igor <112963829+siro4ik@users.noreply.github.com>
2025-09-15 19:43:28 +0300

    Удалены Firebase скрипты из index.html и добавлены в modules/firebase.js

--- public/modules/firebase.js
+++ public/modules/firebase.js
@@ -8,3 +8,9 @@ export const firebaseConfig = {
     appId: "1:346613736019:web:17493f838e994d28e65ca6",
     measurementId: "G-N5V8E7TV4D"
 };
+
+export const app = (firebase.apps && firebase.apps.length)
+    ? firebase.app()
+    : firebase.initializeApp(firebaseConfig);
+
+export const database = firebase.database();
