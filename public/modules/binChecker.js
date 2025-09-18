2b27c40d66f73ec2ea8ce69209ee4ab158c007d5
Igor <112963829+siro4ik@users.noreply.github.com>
2025-09-19 00:45:13 +0300

    подвязка API и query на id-шки

--- public/modules/binChecker.js
+++ public/modules/binChecker.js
@@ -1,14 +1,21 @@
+const lookup = require('binlookup')();
+
 document.querySelector('#binForm').addEventListener('submit', function(e){
     e.preventDefault();
 
     const cardNumber = document.querySelector('#cardNumber').value;
+    const binIin = document.querySelector('#bin-iin');
+    const cardMark = document.querySelector('#card-mark');
+    const bankName = document.querySelector('#bank-name');
 
     const bin = cardNumber.replace(/\D/g, '').substring(0, 6);
+
+ 
 
     if (bin.length < 6){
         alert('Бин номер должен быть корректным!')
     }else{
-        return bin;
+       
     }
 
 
