75290a3591b047b743075e24c30e92e355cbbf71
Igor <deputatov0@mail.ru>
2025-10-06 16:18:40 +0300

    +

--- public/modules/binChecker.js
+++ public/modules/binChecker.js
@@ -2,7 +2,20 @@ import { binData } from "./modules/binDatabase.js";
 
 
 function checkBIN(bin){
-  const cleanBIN = bin.replace(/\D/g, '');
+  let cleanBIN = bin.replace(/\D/g, '');
 
-  if (cleanBIN.includes
+  if (cleanBIN === ""){
+    return {isValid: false, message: 'Введите корректный БИН!'}
+  }
+
+  if (cleanBIN > 6){
+    cleanBIN = cleanBIN.slice(0, 6);
+  }
+
+  const binData = binDatabase[cleanBIN];
+  if(!binData){
+    return{isValid: false, message: 'БИН не найден в базе!'}
+  }
+
+  return{isValid: true, data: binData};
 }
